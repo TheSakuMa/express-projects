9454f6a98237fea733fe81ca6a954a3b924f0ef6
TheSakuMa <thesakumasan@gmail.com>
2020-06-14 11:20:53 +0900

    third commit

--- react-todolist/src/index.js
+++ react-todolist/src/index.js
@@ -1,17 +1,34 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+class TodoList extends React.Component {
+  render() {
+    return (
+      <form className="todolist-form">
+        <input type="text"/>
+      </form>
+    );
+  }
+}
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+class TodoForm extends React.Component {
+  render() {
+    return (
+      <form id="todolist-add-form">
+        <input type="text"/>
+      </form>
+    );
+  }
+}
+
+function App() {
+  return (
+    <div className="todolists">
+      <TodoList />
+      <TodoForm />
+    </div>
+  );
+}
+
+ReactDOM.render(<App />, document.getElementById('root'));
