e44d41fd6a77a9d499cb328da1417d6d95fcbb38
M.Sakurai <thesakumasan@gmail.com>
2020-07-23 11:09:31 +0900

    機能一通り完成

--- react-todolist/src/index.js
+++ react-todolist/src/index.js
@@ -6,12 +6,14 @@ class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
+      text: '',
       items: [],
-      text: ''
-    };
-    // this.changeTodo = this.changeTodo.bind(this);
+    }
     this.handleChange = this.handleChange.bind(this);
     this.addTodo = this.addTodo.bind(this);
+    this.changeTodo = this.changeTodo.bind(this);
+    this.deleteTodo = this.deleteTodo.bind(this);
+    this.toggleTodo = this.toggleTodo.bind(this);
   }
 
   handleChange(e) {
@@ -25,59 +27,100 @@ class App extends React.Component {
     }
     const newItem = {
       text: this.state.text,
-      id: Date.now()
+      id: Date.now(),
+      checkBox: true,
     };
     this.setState(state => ({
       items: state.items.concat(newItem),
-      text: ''
+      text: '',
     }));
   }
 
+  changeTodo(e, index) {
+    e.preventDefault();
+    if (e.target.text.value.length === 0) {
+      return;
+    }
+    let newItems = this.state.items.slice();
+    newItems[index].text = e.target.text.value;
+    this.setState({
+      items: newItems,
+    });
+    document.getElementById('add-todo').text.focus();
+  }
+
+  toggleTodo(e, index) {
+    let newCheckStatus = !(this.state.items[index].checkBox);
+    let newItems = this.state.items.slice();
+    newItems[index].checkBox = newCheckStatus;
+    this.setState({
+      items: newItems,
+    });
+  }
+
+  deleteTodo(e, index) {
+    e.preventDefault();
+    let newItems = this.state.items.slice();
+    newItems.splice(index, 1);
+    this.setState({
+      items: newItems,
+    });
+  }
+
   render() {
+    /* mapメソッドでitemsを展開し、要素分のTodoコンポーネントを呼び出す */
+    const todos = this.state.items.map((item, index) => {
+      return (
+        <Todo 
+          key={item.id} item={item} index={index} onSubmitCallBack={this.changeTodo} onDeleteCallBack={this.deleteTodo} onToggleCallBack={this.toggleTodo}
+        />
+      );
+    });
+    
     return (
       <main>
         <h3>TODO</h3>
         <div>
           {this.state.items.length}個のタスクがあります。
         </div>
-        <TodoList items={this.state.items}/>
+        <div id="todos-div">
+          {todos}
+        </div>
         <form id="add-todo" onSubmit={this.addTodo}>
-          <label htmlFor="new-todo">
-            What needs to be done?
-          </label>
-          <input id="new-todo" name="text" onChange={this.handleChange} value={this.state.text}/>
+          <input type="text" name="text" onChange={this.handleChange} value={this.state.text}/>
           <button>
             Add #{this.state.items.length + 1}
           </button>
         </form>
       </main>
-    );
+    )
   }
 }
 
-class TodoList extends React.Component {
-  changeTodo(e, index) {
-    e.preventDefault();
-    if (e.target.text.value.length === 0) {
-      return;
-    }
-    this.props.items[index].text = e.target.text.value;
-    this.setState({
-      items: this.props.items.slice(),
-    });
-    document.getElementById('add-todo').text.focus();
+class Todo extends React.Component {
+  /* 親要素のイベントハンドラを呼び出すメソッドを定義 */
+  submitEvent(e, index) {
+    this.props.onSubmitCallBack(e, index);
+    return;
+  }
+  clickEvent(e, index) {
+    this.props.onDeleteCallBack(e, index);
+    return;
+  }
+  checkEvent(e, index) {
+    this.props.onToggleCallBack(e, index);
+    return;
   }
 
   render() {
     return (
-      <div className="todolist-div">
-        {this.props.items.map((item, index) => (
-          <form key={item.id} onSubmit={e => this.changeTodo(e, index)}>
-            <input type="checkbox"/>
-            <input type="text" name="text" defaultValue={item.text}/>
-          </form>
-        ))}
-      </div>
+      <form key={this.props.item.id} onSubmit={e => this.submitEvent(e, this.props.index)}>
+        <div className={this.props.item.checkBox? "checkbox": "checkbox checked"} onClick={e => this.checkEvent(e, this.props.index)}></div>
+        <input type="text" name="text" defaultValue={this.props.item.text}/>
+        <button type="button" onClick={e => this.clickEvent(e, this.props.index)}>
+          delete
+        </button>
+      </form>
     );
   }
 }
