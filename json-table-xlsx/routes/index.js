b8b362902598da7eb55ca157a816c7d846c21e79
TheSakuMa <thesakumasan@gmail.com>
2021-03-21 19:56:37 +0900

    json-table-xlsx: initial commit

---
+++ json-table-xlsx/routes/index.js
@@ -0,0 +1,25 @@
+var express = require('express');
+var router = express.Router();
+var axios = require('axios');
+
+const title = 'JSONデータをテーブルとして出力しよう'
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: title, data: null });
+});
+
+router.post('/execute', function(req, res, next) {
+  var url = req.body.url;
+  axios.get(url)
+  .then(function(response) {
+    res.render('index', { title: title, data: JSON.stringify(response.data, null, "  ") });
+    console.log(response.data);
+  })
+  .catch(function(err) {
+    console.error(err);
+    res.render('index', { title: title, data: 'エラーが発生しました。' });
+  });
+});
+
+module.exports = router;
